16bd7c052556aa41fab657d9e001dc67449b5b77
chauzar_vanneeza <cha.vanneeza@gmail.com>
2023-05-17 16:25:41 +0700

    First Commit

--- auth/auth.go
+++ auth/auth.go
@@ -1,31 +1,36 @@
 package auth
 
 import (
-	"log"
 	"net/http"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
 // Buaat secret key
-var jwtKey = []byte("SECRET_KEY_BEBAS")
+var jwtKey = "SANGAT_RAHASIA"
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		tokenString := c.GetHeader("Authorization")
 
 		if tokenString == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorization"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			c.Abort()
 			return
 		}
+
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) { return jwtKey, nil })
 
 		if !token.Valid || err != nil {
-			log.Println(token)
-			log.Println(jwtKey)
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorization"})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
 			c.Abort()
 			return
 		}
@@ -35,4 +40,42 @@ func AuthMiddleware() gin.HandlerFunc {
 
 		c.Next()
 	}
+}
+
+func LoginHandler(c *gin.Context) {
+	var user User
+	if err := c.ShouldBind(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// logic authentication (compare username dan password)
+	if user.Username == "enigma" && user.Password == "12345" {
+		// bikin code untuk generate token
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		claims := token.Claims.(jwt.MapClaims)
+
+		claims["username"] = user.Username
+		claims["exp"] = time.Now().Add(time.Minute * 1).Unix() // token akan expired dalam 1 menit
+
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to generate token"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"token": tokenString}) // jika login berhasil, dapatkan token string
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+	}
+}
+
+func ProfileHandler(c *gin.Context) {
+	// ambil username dari JWT token
+	claims := c.MustGet("claims").(jwt.MapClaims)
+	username := claims["username"].(string)
+
+	// seharusnya return user dari database, tapi di contoh ini kita return username
+	c.JSON(http.StatusOK, gin.H{"username": username})
 }
